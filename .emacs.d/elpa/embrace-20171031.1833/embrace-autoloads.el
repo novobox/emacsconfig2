f1a2c9ddf8e53c1976aeeed8f46720566aa0036e
arnaud <arnaud@websiteburo.com>
2018-09-23 00:20:45 +0200

    Ajout melpa embrace region et er/expand-region

---
+++ .emacs.d/elpa/embrace-20171031.1833/embrace-autoloads.el
@@ -0,0 +1,62 @@
+;;; embrace-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "embrace" "embrace.el" (23462 48395 646372
+;;;;;;  577000))
+;;; Generated autoloads from embrace.el
+
+(autoload 'embrace-delete "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-change "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-add "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-commander "embrace" "\
+
+
+\(fn)" t nil)
+
+(autoload 'embrace-LaTeX-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-org-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-ruby-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+(autoload 'embrace-emacs-lisp-mode-hook "embrace" "\
+
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("embrace-pkg.el") (23462 48395 653193
+;;;;;;  701000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; embrace-autoloads.el ends here
